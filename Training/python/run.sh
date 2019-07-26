91eff1b8b228597878f6ceb8d35b76d192f57760
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-07-26 15:41:25 +0200

    add all files

--- Training/python/run.sh
+++ Training/python/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for FILE in tau_HTT.h5 e_DY.h5 mu_DY.h5 jet_DY.h5
+for FILE in tau_HTT.h5 e_DY.h5 mu_DY.h5 jet_DY.h5 jet_QCD.h5 e_W.h5 mu_W.h5 jet_W.h5
 do
     python analyze_gradients.py --input ../../../data/${FILE} --model ../../../model/DeepTau2017v2p6_step1_e6.pb --output .
 done
