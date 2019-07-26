94d3b72b4d1c54f2de244489a5c233f4187180c6
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-07-26 15:37:04 +0200

    fix typo

--- Training/python/run.sh
+++ Training/python/run.sh
@@ -3,5 +3,5 @@
 
 for FILE in tau_HTT.h5 e_DY.h5 mu_DY.h5 jet_DY.h5
 do
-    python analyze_gradients.py --input ../../../data/${FILE}.h5 --model ../../../model/DeepTau2017v2p6_step1_e6.pb --output .
+    python analyze_gradients.py --input ../../../data/${FILE} --model ../../../model/DeepTau2017v2p6_step1_e6.pb --output .
 done
