0f9cb4b00fd45274aa031a505ed24cb9a014808f
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-07-26 15:13:33 +0200

    add propagation to input layer

--- Training/python/analyze_gradients.py
+++ Training/python/analyze_gradients.py
@@ -41,6 +41,10 @@ class Predictor:
         self.grad_mu = tf.gradients(self.y_graph[:, mu], self.concat_graph)[0]
         self.grad_tau = tf.gradients(self.y_graph[:, tau], self.concat_graph)[0]
         self.grad_jet = tf.gradients(self.y_graph[:, jet], self.concat_graph)[0]
+        self.grad_tauin_e = tf.gradients(self.y_graph[:, e], self.x_graphs[0])[0]
+        self.grad_tauin_mu = tf.gradients(self.y_graph[:, mu], self.x_graphs[0])[0]
+        self.grad_tauin_tau = tf.gradients(self.y_graph[:, tau], self.x_graphs[0])[0]
+        self.grad_tauin_jet = tf.gradients(self.y_graph[:, jet], self.x_graphs[0])[0]
 
     def Predict(self, session, X):
         if len(self.x_graphs) != len(X):
@@ -49,7 +53,7 @@ class Predictor:
         feed_dict = {}
         for n in range(len(self.x_graphs)):
             feed_dict[self.x_graphs[n]] = X[n]
-        pred, grad_e, grad_mu, grad_tau, grad_jet = session.run([self.y_graph, self.grad_e, self.grad_mu, self.grad_tau, self.grad_jet], feed_dict=feed_dict)
+        pred, grad_e, grad_mu, grad_tau, grad_jet, grad_tauin_e, grad_tauin_mu, grad_tauin_tau, grad_tauin_jet = session.run([self.y_graph, self.grad_e, self.grad_mu, self.grad_tau, self.grad_jet, self.grad_tauin_e, self.grad_tauin_mu, self.grad_tauin_tau, self.grad_tauin_jet], feed_dict=feed_dict)
         if np.any(np.isnan(pred)):
             raise RuntimeError("NaN in predictions. Total count = {} out of {}".format(
                                np.count_nonzero(np.isnan(pred)), pred.shape))
@@ -60,7 +64,7 @@ class Predictor:
         return pandas.DataFrame(data = {
             'deepId_e': pred[:, e], 'deepId_mu': pred[:, mu], 'deepId_tau': pred[:, tau],
             'deepId_jet': pred[:, jet]
-        }), np.sum(np.abs(grad_e), axis=0), np.sum(np.abs(grad_mu), axis=0), np.sum(np.abs(grad_tau), axis=0), np.sum(np.abs(grad_jet), axis=0)
+        }), np.sum(np.abs(grad_e), axis=0), np.sum(np.abs(grad_mu), axis=0), np.sum(np.abs(grad_tau), axis=0), np.sum(np.abs(grad_jet), axis=0), np.sum(np.abs(grad_tauin_e), axis=0), np.sum(np.abs(grad_tauin_mu), axis=0), np.sum(np.abs(grad_tauin_tau), axis=0), np.sum(np.abs(grad_tauin_jet), axis=0)
 
 if args.filelist is None:
     if os.path.isdir(args.input):
@@ -90,6 +94,10 @@ grad_e = np.zeros(185, dtype=np.float32)
 grad_mu = np.zeros(185, dtype=np.float32)
 grad_tau = np.zeros(185, dtype=np.float32)
 grad_jet = np.zeros(185, dtype=np.float32)
+grad_tauin_e = np.zeros(47, dtype=np.float32)
+grad_tauin_mu = np.zeros(47, dtype=np.float32)
+grad_tauin_tau = np.zeros(47, dtype=np.float32)
+grad_tauin_jet = np.zeros(47, dtype=np.float32)
 for file_name in file_list:
     if args.max_n_files is not None and file_index >= args.max_n_files: break
     full_name = prefix + file_name
@@ -109,11 +117,15 @@ for file_name in file_list:
 
     with tqdm(total=loader.data_size, unit='taus') as pbar:
         for inputs in loader.generator(return_truth = False, return_weights = False):
-            df, grad_e_, grad_mu_, grad_tau_, grad_jet_ = predictor.Predict(sess, inputs)
+            df, grad_e_, grad_mu_, grad_tau_, grad_jet_, grad_tauin_e_, grad_tauin_mu_, grad_tauin_tau_, grad_tauin_jet_ = predictor.Predict(sess, inputs)
             grad_e += grad_e_
             grad_mu += grad_mu_
             grad_tau += grad_tau_
             grad_jet += grad_jet_
+            grad_tauin_e += grad_tauin_e_
+            grad_tauin_mu += grad_tauin_mu_
+            grad_tauin_tau += grad_tauin_tau_
+            grad_tauin_jet += grad_tauin_jet_
             num_inputs += df.shape[0]
             read_hdf_lock.acquire()
             df.to_hdf(pred_output, args.tree, append=True, complevel=1, complib='zlib')
@@ -129,6 +141,10 @@ grad_e /= float(num_inputs)
 grad_mu /= float(num_inputs)
 grad_tau /= float(num_inputs)
 grad_jet /= float(num_inputs)
+grad_tauin_e /= float(num_inputs)
+grad_tauin_mu /= float(num_inputs)
+grad_tauin_tau /= float(num_inputs)
+grad_tauin_jet /= float(num_inputs)
 """
 grad_e /= np.sum(grad_e)
 grad_mu /= np.sum(grad_mu)
@@ -140,8 +156,9 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(12, 4))
+plt.figure(figsize=(6, 6))
 
+plt.subplot(211)
 x = range(185)
 plt.plot(x, grad_e, "o-", alpha=0.7, label="e")
 plt.plot(x, grad_mu, "o-", alpha=0.7, label="mu")
@@ -149,17 +166,26 @@ plt.plot(x, grad_tau, "o-", alpha=0.7, label="tau")
 plt.plot(x, grad_jet, "o-", alpha=0.7, label="jet")
 plt.axvline(x=57, lw=1, color="k")
 plt.axvline(x=57+64, lw=1, color="k")
-plt.xlabel("Nodes of concat. layer")
+plt.xlabel("Nodes of the concat. layer")
 plt.ylabel("Mean abs. of the gradient")
 """
 plt.text(25, 0.002, "tau")
 plt.text(85, 0.002, "inner")
 plt.text(150, 0.002, "outer")
 """
-plt.text(25, 0.002, "tau")
 plt.xlim((0, 184))
-plt.tight_layout()
 plt.legend()
+
+plt.subplot(212)
+x = range(47)
+plt.plot(x, grad_tauin_e, "o-", alpha=0.7, label="e")
+plt.plot(x, grad_tauin_mu, "o-", alpha=0.7, label="mu")
+plt.plot(x, grad_tauin_tau, "o-", alpha=0.7, label="tau")
+plt.plot(x, grad_tauin_jet, "o-", alpha=0.7, label="jet")
+plt.xlabel("Nodes of the tau input layer")
+plt.legend()
+
+plt.tight_layout()
 plt.savefig("grad_" + os.path.basename(file_name).replace(".h5", "") + ".png")
 
 print("All files processed.")
